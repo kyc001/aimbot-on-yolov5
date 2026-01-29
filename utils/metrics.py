f361656bdd0d0b1cbd1612e6834ec9aef3661d34
kyc001 <852117630@qq.com>
2026-01-29 22:12:19 +0800

    fix: NumPy 2.0 compatibility - replace deprecated np.trapz with np.trapezoid

--- utils/metrics.py
+++ utils/metrics.py
@@ -113,7 +113,9 @@ def compute_ap(recall, precision):
     method = "interp"  # methods: 'continuous', 'interp'
     if method == "interp":
         x = np.linspace(0, 1, 101)  # 101-point interp (COCO)
-        ap = np.trapz(np.interp(x, mrec, mpre), x)  # integrate
+        # NumPy 2.0+ renamed trapz to trapezoid
+        trapz_fn = getattr(np, 'trapezoid', np.trapz) if hasattr(np, 'trapz') else np.trapezoid
+        ap = trapz_fn(np.interp(x, mrec, mpre), x)  # integrate
     else:  # 'continuous'
         i = np.where(mrec[1:] != mrec[:-1])[0]  # points where x axis (recall) changes
         ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])  # area under curve
